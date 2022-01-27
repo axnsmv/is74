1e7dc6bcbbcdd9062a2fc2e9e6904cced4e12c55
Axnsmv <sana_74@mail.ru>
2022-01-27 11:23:56 +0500

    add sync.WaitGroup

--- main.go
+++ main.go
@@ -5,40 +5,75 @@ import (
 	"io/ioutil"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+var oldlUrls = map[string]bool{}
+
 func UrlsRead() []string {
+	var retUrls []string
 	buff, err := ioutil.ReadFile("./urls.txt")
 	if err != nil {
 		panic(err)
 	}
-	return strings.Split(string(buff), "\n")
+	urls := strings.Split(string(buff), "\n")
+	for _, url := range urls {
+		if !oldlUrls[url] {
+			oldlUrls[url] = true
+			retUrls = append(retUrls, url)
+		}
+	}
+	return retUrls
+}
+
+func dialTimeout(network, addr string) (net.Conn, error) {
+	return net.DialTimeout(network, addr, time.Duration(5*time.Second))
 }
 
 func main() {
-	f, err := os.OpenFile("datalog.log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		panic(err)
+	t := 10
+	transport := http.Transport{
+		Dial: dialTimeout,
 	}
+	client := http.Client{
+		Transport: &transport,
+	}
+	var wg sync.WaitGroup
+	for {
+		fmt.Println("=====================================")
+		f, _ := os.OpenFile("datalog.log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		urls := UrlsRead()
 
-	defer f.Close()
-	urls := UrlsRead()
-	for _, urlSpace := range urls {
-		url := strings.TrimSpace(urlSpace)
-		res, _ := http.Get(url)
-		urlNoHttp := strings.Split(url, "//")[1]
-		conn, _ := net.Dial("tcp", fmt.Sprintf("%s:80", urlNoHttp))
-		ip := strings.ReplaceAll(conn.RemoteAddr().String(), ":80", "")
-		doc, err := goquery.NewDocumentFromReader(res.Body)
-		if err != nil {
-			panic(err)
+		for i, urlSpace := range urls {
+			urlTrim := strings.TrimSpace(urlSpace)
+			if i != 0 && i%t == 0 {
+				wg.Add(t)
+				wg.Wait()
+			}
+			go func(_url string, _i int) {
+				defer wg.Done()
+				urlParse, _ := url.Parse(_url)
+				address, _ := net.LookupIP(urlParse.Host)
+				res, err := client.Get(_url)
+				str := ""
+				if err == nil {
+					doc, _ := goquery.NewDocumentFromReader(res.Body)
+					title := doc.Find("title").Text()
+					str = fmt.Sprintf("%d|%s|%s|%d|%s", _i, urlParse.Host, address[0], res.StatusCode, title)
+					fmt.Println(str)
+				} else {
+					str = fmt.Sprintf("%d|%s|%d", _i, urlParse.Host, 500)
+					fmt.Println(str)
+				}
+				f.WriteString(str)
+			}(urlTrim, i)
 		}
-		title := doc.Find("title").Text()
-		str := fmt.Sprintf("%s | %s | %d | %s\n", urlNoHttp, ip, res.StatusCode, title)
-		f.WriteString(str)
+		f.Close()
 	}
 }
